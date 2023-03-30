3d320ebd4464dedccb23ec79fd0819cf41e7adfe
Mukesh Singh <92354863+mukesh1150935@users.noreply.github.com>
2023-03-30 11:39:43 +0530

    db updated2

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -6,7 +6,7 @@ const password = process.env.DB_PASSWORD;
 const storage = new GridFsStorage({
     // url: `mongodb://${username}:${password}@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`,
     // url: process.env.DB,
-    url: 'mongodb+srv://rehan:21bcs049@cluster0.jxhydap.mongodb.net/?retryWrites=true&w=majority',
+    url:`mongodb+srv://mukesh_singh_09:Msr1150935@cluster0.gajprnv.mongodb.net/?retryWrites=true&w=majority`
     options: { useNewUrlParser: true },
     file: (request, file) => {
     
